e82f5225a9d983cfe732c8b325580dd17a730dc9
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-09-03 03:11:19 -0700

    Semantic update button

--- js/main.js
+++ js/main.js
@@ -160,9 +160,9 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('button');
   more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
+  more.onclick = () => window.location = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
   return li
