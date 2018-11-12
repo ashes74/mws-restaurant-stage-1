7d289c3cfca2b9ee41e9b3dde5bdf326412c1033
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-11-12 01:15:47 -0800

    Set autoprefixer options

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const gulp = require("gulp");
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task("styles", function() {
     gulp
       .src("src/sass/**/*.scss")
       .pipe(sass())
       .on("error", sass.logError)
+      .pipe(
+        autoprefixer({
+          browsers: ["last 2 versions"]
+        })
+      )
       .pipe(gulp.dest("dist/css"));
   });
 
