005144ecfd1ca3885c476115f2882710d2ee3845
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-11-12 01:25:34 -0800

    Add browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require("gulp");
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+gulp.task("default", ["styles"], function(){
+    gulp.watch("src/sass/**/*.scss", ["styles"]);
+    browserSync.init({
+        server: "./"
+      });
+})
 
 gulp.task("styles", function() {
     gulp
@@ -12,9 +20,7 @@ gulp.task("styles", function() {
           browsers: ["last 2 versions"]
         })
       )
-      .pipe(gulp.dest("dist/css"));
+      .pipe(gulp.dest("dist/css"))
+      .pipe(browserSync.stream());
   });
 
-gulp.task("default", function(){
-
-})
