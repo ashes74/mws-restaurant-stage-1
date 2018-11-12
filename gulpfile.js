89407d21cccede55aa7e251012825b3a51f2c4a6
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-11-12 01:52:22 -0800

    Add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ const gulp = require("gulp");
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
+const eslint = require('gulp-eslint');
 
 gulp.task("default", ["styles"], function(){
-    gulp.watch("src/sass/**/*.scss", ["styles"]);
+    gulp.watch("/sass/**/*.scss", ["styles"]);
+    gulp.watch('/js/**/*.js', ['lint']);
     browserSync.init({
         server: "./src"
       });
@@ -24,3 +26,19 @@ gulp.task("styles", function() {
       .pipe(browserSync.stream());
   });
 
+  //copied from course materials 
+  gulp.task('lint', function() {
+    return (
+        gulp
+            .src(['src/js/**/*.js'])
+            // eslint() attaches the lint output to the eslint property
+            // of the file object so it can be used by other modules.
+            .pipe(eslint())
+            // eslint.format() outputs the lint results to the console.
+            // Alternatively use eslint.formatEach() (see Docs).
+            .pipe(eslint.format())
+            // To have the process exit with an error code (1) on
+            // lint error, return the stream and pipe to failOnError last.
+            .pipe(eslint.failOnError())
+    );
+});
