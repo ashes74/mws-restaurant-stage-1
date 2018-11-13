14d2240501ee1f8a4cdba51f2538f26248a32a9f
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-11-13 09:13:50 -0800

    Add reload on change

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,54 +1,62 @@
 'use strict';
 
-import {watch, src, dest, parallel} from "gulp";
+import {watch, src, dest, parallel, series} from "gulp";
 import sass, {logError} from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
-import browserSync from 'browser-sync';
 import eslint, {format, failOnError} from 'gulp-eslint';
 import jest from 'gulp-jest';
 
+const browserSync = require('browser-sync').create();
 
-export function start() {
-    const stylesWatcher = watch('/sass/**/*.scss', styles);
-    const lintWatcher = watch('/js/**/*.js', lint);
-    const copyHtmlWatcher = watch('/src/index.html', copyHtml);
-    
-    stylesWatcher.on('all',(...args)=>{
-        console.log(args);
-    })
-    lintWatcher.on('all',(...args)=>{
-        console.log(args);
-    })
-    copyHtmlWatcher.on('all',(...args)=>{
-        console.log(args);
-    })
-    browserSync({
-        server: './dist',
-    });
+const paths = {
+    js: {
+        src: 'src/**/*.js',
+        dest: 'dist/'
+    },
+    sw: {
+        src: 'src/',
+        dest: 'dist/'
+    },
+    html: {
+        src: 'src/**/*.html',
+        dest: 'dist/'
+    }
 }
 
-export default parallel( copyHtml, copyImages, styles, lint, copyJS, copyData, start)
+export function sync() {
+    browserSync.init({
+        port: 3000,
+        server: {
+            baseDir: './dist'
+        }
+    });
+    watch('/sass/**/*.scss', styles).on('change', browserSync.reload);
+    watch('/js/**/*.js', series(lint, copyJS)).on('change', browserSync.reload);
+    watch('/src/index.html', copyHtml).on('change', browserSync.reload);
+}
+
+export default parallel(copyHtml, copyImages, styles, lint, copyJS, copyData, sync)
 
 //TODO: get data from server
-function copyData (){
+function copyData() {
     return src('src/data/restaurants.json').pipe(dest('./dist/data'))
 }
 
-export function copyHtml () {
-   return src(['./src/**/*.html', './src/sw.js']).pipe(dest('./dist', {overwrite: true}))
+export function copyHtml() {
+    return src(['./src/**/*.html', './src/sw.js']).pipe(dest('./dist', {overwrite: true}))
 }
 
 //TODO: minify and concatenate js
-export function copyJS () {
-   return src('./src/js/**/*.js').pipe(dest('./dist/js', {overwrite: true}))
+export function copyJS() {
+    return src('./src/js/**/*.js').pipe(dest('./dist/js', {overwrite: true}))
 }
 
 export function copyImages() {
-   return src('./src/img/*').pipe(dest('./dist/img'))
+    return src('./src/img/*').pipe(dest('./dist/img'))
 }
 
-export function styles () {
-   return src('src/sass/**/*.scss')
+export function styles() {
+    return src('src/sass/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .on('error', logError)
         .pipe(autoprefixer({browsers: ['last 2 versions']}))
@@ -57,7 +65,7 @@ export function styles () {
 }
 
 //copied from course materials
-export function lint () {
+export function lint() {
     return (src(['src/js/**/*.js'])
     // eslint() attaches the lint output to the eslint property of the file object
     // so it can be used by other modules.
@@ -70,7 +78,7 @@ export function lint () {
         .pipe(failOnError()));
 }
 
-export function test () {
+export function test() {
     return src('__tests__').pipe(jest({
         "verbose": true,
         "preprocessorIgnorePatterns": ["./dist/", "./node_modules/"]
