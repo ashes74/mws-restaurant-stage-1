58098c4d5a912e63898f4ae2a7bdfac89b6f6386
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-11-14 03:48:05 -0800

    update cached links

--- src/sw.js
+++ src/sw.js
@@ -1,12 +1,12 @@
 const staticCacheName = "mws-rest-reviews-v2"
 const urlsToCache = [
     '/',
-    'js/main.js',
-    'js/restaurant_info.js',
-    'js/dbhelper.js',
-    'css/styles.css',
-    'css/mobile.css',
-    'css/under800.css',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/js/dbhelper.js',
+    '/css/styles.css',
+    '/css/mobile.css',
+    '/css/under800.css',
     '/404.html',
     '/offline.html',
     '/img/notfound.jpg',
