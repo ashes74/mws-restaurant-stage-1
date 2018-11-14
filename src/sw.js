3ee9b3e8cc82a830b3f428953bcbb395581f09ac
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-11-14 04:25:29 -0800

    Revert to async

--- src/sw.js
+++ src/sw.js
@@ -16,46 +16,46 @@ const urlsToCache = [
 ]
 
 self.addEventListener('install', event => {
+    console.log('Service worker installing...');
     // console.log('Attempting to install service worker and cache static assets ');
-    event.waitUntil(caches.open(staticCacheName).then(async cache => {
-        try {
-            return cache.addAll(urlsToCache);
-        } catch (err) {
-            console.error("Cache open failure", err);
-        }
-    }))
+    event.waitUntil(
+        caches.open(staticCacheName)
+        .then(cache => cache.addAll(urlsToCache))
+        .catch(err => {console.error("Cache open failure", err)})
+    )
 })
 
 self.addEventListener('activate', event => {
-    // console.log(`Activating service worker`);
-    event.waitUntil(caches.keys().then(cacheNames => {
-        return Promise.all(cacheNames.filter(cacheName => {
-            return cacheName.startsWith('mws-rest-reviews-') && cacheName != staticCacheName;
-        }).map(cacheName => caches.delete(cacheName)))
-    }).catch(err => console.error("Error activating service worker", err)))
+    console.log('Service worker activating...');
+    event.waitUntil(
+        caches.keys()
+        .then(cacheNames => {
+        return Promise.all(
+            cacheNames
+            .filter(cacheName => {
+                return cacheName.startsWith('mws-rest-reviews-') && cacheName != staticCacheName;
+            })
+            .map(cacheName => caches.delete(cacheName))
+        )       
+    }).catch(err => console.error(`Error activating service worker ${err}`)))
 })
 
 self.addEventListener('fetch', event => {
     // console.log('Fetch event for ', event.request.url);
-    event.respondWith(caches.match(event.request).then(cachedResponse => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(cachedResponse => {
         // Find and return cached version of the page
-        if (cachedResponse) {
-            return cachedResponse;
-        }
+        if (cachedResponse) return cachedResponse;
         // No match found in cache => fetch page via network request
-        return fetch(event.request, {
-            method: 'GET',
-            mode: 'cors'
-        }).then(async response => {
+        return fetch(event.request)
+        .then(response => {
             // No match found on network => page does not exist return 404
-            if (response.status === 404) {
-                return caches.match('/404.html');
-            }
-            const cache = await caches.open(staticCacheName);
-            cache.put(event.request.url, response.clone());
+            if (response.status === 404) return caches.match('/404.html');
             return response;
         }).catch(error => {
             // Fetch from network didn 't work, unable to access network send offline page
+            console.error(error);
             return caches.match('/offline.html');
         })
     }))
