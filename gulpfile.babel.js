013cda29af1930a0f4ff24f5400dbc6efb699d67
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-11-14 03:47:10 -0800

    Clean up gulp file,: bundle lint in copyJS rename styles to sass, etc.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -20,11 +20,11 @@ const paths = {
         dest: 'dist/'
     },
     html: {
-        src: 'src/**/*.html',
+        src: 'src/*.html',
         dest: 'dist/'
     },
     sass: {
-        src: 'src/sass/**/*.scss',
+        src: 'src/sass/*.scss',
         dest: 'dist/css'
     },
     images: {
@@ -32,60 +32,62 @@ const paths = {
         dest: 'dist/img'
     },
     app: {
-        src: 'src',
-        dest: 'dist'
+        src: 'src/**',
+        dest: 'dist/',
+       
     }
 }
 
 export function _watch() {
-    watch(paths.sass.src, {}, styles);
-    watch(paths.js.src, {}, series(lint, copyJS));
+    watch(paths.sass.src, {}, sass);
+    watch(paths.js.src, {}, copyJS);
     watch(paths.html.src, {}, copyHtml);
 }
 
 export function sync() {
     _watch()
     browserSync.init({
-        port: 3000,
+        port: 8000,
         server: {
             baseDir: './dist'
         }
     });
 }
 
-export default series(clean, parallel(copyHtml, copyImages, styles, lint, copyJS), sync)
+export default series(clean, parallel(copy, sass), sync)
 
-export function copyHtml() {
+export function copy() {
+    copyJS()
+    copyHtml()
+    return src(paths.app.src, { base: './src', ignore: [`src/sass/**`, paths.html.src, paths.js.src ]})
+    .pipe(dest(paths.app.dest))
+    .pipe(browserSync.stream())
+}
+
+function copyHtml() {
     return src(paths.html.src)
-        .pipe(dest(paths.html.dest, { overwrite: true }))
-        .pipe(browserSync.stream({ match: "**/*.html" }))
+    .pipe(dest(paths.html.dest))
+    .pipe(browserSync.stream({ match: "**/*.html" }))
 }
 
-//TODO: minify and concatenate js
-export function copyJS() {
-    return src(paths.js.src, {base:'./src'})
-        .pipe(dest(paths.js.dest, { overwrite: true }))
-        .pipe(browserSync.stream({ match: "**/*.js" }))
+function copyJS() {
+    return src(paths.js.src, {base: './src'})
+    .pipe(dest(paths.js.dest))
+    // start of lint step 
+    .pipe(eslint()) 
+    .pipe(format())
+    .pipe(failOnError())
+    // end of lint step
+    .pipe(browserSync.stream({ match: "**/*.js" }))
 }
 
-export function copyImages() {
-    return src(paths.images.src).pipe(dest(paths.images.dest))
-}
-
-export function styles() {
+export function sass() {
     return src(paths.sass.src)
         .pipe(_sass({ outputStyle: 'compressed' }))
         .on('error', logError)
         .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
         .pipe(dest(paths.sass.dest))
         .pipe(browserSync.stream({ match: "**/*.css" }));
-}
-
-export function lint() {
-    return (src([paths.js.src])
-        .pipe(eslint())
-        .pipe(format())
-        .pipe(failOnError()))
 }
 
 export function test() {
