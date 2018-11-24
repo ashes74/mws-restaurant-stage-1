d882ed2a1a3013d9d2ebecd25dcb304dd1d8baff
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-11-24 02:08:09 -0800

    Update service worker registration based on: https://developers.google.com/web/fundamentals/primers/service-workers/registration#conclusion

--- src/register.js
+++ src/register.js
@@ -3,11 +3,20 @@
  */
 
 (() => {
-    if (navigator.serviceWorker){
-        navigator
-            .serviceWorker
-            .register('/sw.js')
-            .then(registration => console.log(`Registration successful`, registration))
-            .catch(err => console.log(`Registratation failed ${err}`))
+    if (navigator.serviceWorker) {
+        window.addEventListener('load', function () {
+            navigator
+                .serviceWorker
+                .register('/sw.js')
+                .then(registration => console.log(`Registration successful`, registration))
+                .catch(err => console.log(`Registratation failed ${err}`))
+        })
     }
 })()
+
+//// From https://www.npmjs.com/package/serviceworker-webpack-plugin
+// import runtime from 'serviceworker-webpack-plugin/lib/runtime';
+ 
+// if ('serviceWorker' in navigator) {
+//   const registration = runtime.register();
+// }
