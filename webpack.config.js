8b486e5e596aee2a7e552103f3ea084c26ce18d6
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-11-24 00:18:38 -0800

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: "./src/js/main.js",
+        restaurant: "./src/js/restaurant_info.js"
+    },
+    output: {
+        path: __dirname + '/dist',
+        // filename: "[name].[chunkhash:8].js"
+        filename: "[name].js"
+    },
+    devtool: "source-map",
+    devServer: {
+        contentBase: `${__dirname}/dist`,
+        hot: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                enforce: "pre",
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "eslint-loader"
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new CopyWebpackPlugin([
+            {
+              from: './src/**/**.html',
+              to: '[name].html',
+            },
+            {
+              from: './src/img/**.*',
+              to: 'img/[name].[ext]',
+            },
+
+
+          ]),
+
+        // new HtmlWebpackPlugin()
+    ],
+    // watch: true
+}
