5e63640078326eec55659b69faf0b67d323ec66e
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-11-28 00:32:11 -0800

    Add blendid

---
+++ config/task-config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  html: false,
+  images: true,
+  fonts: false,
+  static: true,
+  svgSprite: false,
+  ghPages: false,
+  stylesheets: true,
+
+  javascripts: {
+    entry: {
+      // files paths are relative to javascripts.dest in path-config.json
+      main: ["./main.js"],
+      restaurant: ["./restaurant_info.js"]
+    },
+    babel: {
+      presets: ["@babel/preset-env"],
+      plugins: ['@babel/plugin-transform-runtime']
+    }
+
+  },
+
+  html: {
+    alternateTask: function (gulp, PATH_CONFIG, TASK_CONFIG) {
+      return function () {
+        return gulp
+          .src('./src/*.html')
+          .pipe(gulp.dest('./dist/'))
+      }
+    }
+  },
+
+  browserSync: {
+    server: {
+      // should match `dest` in path-config.json
+      baseDir: 'dist'
+    }
+  },
+
+  production: {
+    rev: true
+  }
+}
