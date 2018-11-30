384683290614323db10157f9dda166787b31a3b1
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-11-30 00:02:49 -0800

    Clean up service worker logic: *Add main PWA icons to cache (512 and 192) * Explicitly define fallthrough for DBHelper handling API requests * Explicitly handle requests for restaurant pages * Better handle fetch from network

--- src/static/sw.js
+++ src/static/sw.js
@@ -9,7 +9,9 @@ const urlsToCache = [
     '/restaurant.html',
     '/img/notfound.jpg',
     '/img/favicon.png',
-    '/img/offline.jpg'
+    '/img/offline.jpg',
+    'img/icons/icon-192x192.png',
+    'img/icons/icon-512x512.png'
 ]
 
 self.addEventListener('install', event => {
@@ -29,45 +31,45 @@ self.addEventListener('activate', event => {
 })
 
 self.addEventListener('fetch', event => {
-
     const requestUrl = new URL(event.request.url);
     console.log(`Fetching ${requestUrl}`);
 
-    event.respondWith(caches.open(staticCacheName).then(async cache => {
-        if (requestUrl.pathname.match(/^\/restaurant.html*/)) {
-            return cache.match('/restaurant.html')
+    // if accessing API, default to DB Helper functions
+    if (requestUrl.port === '1337' && requestUrl.hostname === 'localhost') 
+        return console.log("Leave it to DBHelper");
+    
+    //for restaurant pages return restaurant skeleton
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname.match(/^\/restaurant*/)) {
+            console.log('Returning cached skeleton of restaurant page');
+            return event.respondWith(caches.match('/restaurant.html'));
         }
-        try {
-            const cachedResponse = await cache.match(event.request.url);
-            return cachedResponse || await fetchFromNetwork(event.request);
-        } catch (error) {
-            console.error(`Cache miss:`, error);
-            return fetchFromNetwork(event.request);
-        }
-    }, err => {
-        console.error(`Cache open failure:`, err)
-        new Response(`Cache open failure: `, err)
-    }))
-})
+    }
+    // for all other requests return cached value or fetch from network
+    event.respondWith(caches.match(event.request).then(cachedResponse => {
+        return cachedResponse || fetchFromNetwork(event.request);
+    }));
+});
 
 // No match found in cache => fetch page via network request
 async function fetchFromNetwork(request) {
-    console.log(`SW: Fetching from network ${request.url}`);
-
+    // console.log(`SW: Fetching from network ${request.url}`);
     try {
         const networkResponse = await fetch(request);
         // No match found on network => page does not exist return 404
-        const cache = await caches.open(staticCacheName)
         if (networkResponse.status === 404) {
-            return cache.match('/404.html');
+            return caches.match('/404.html');
         }
-        if (request.method === 'GET' && networkResponse.ok) 
-            cache.put(request, networkResponse.clone());
-        return networkResponse || cache.match('/offline.html');
+        if (new URL(request.url).origin === location.origin) {
+            if (request.method === 'GET' && networkResponse.ok) {
+                const cache = await caches.open(staticCacheName)
+                cache.put(request, networkResponse.clone());
+            }
+        }
+        return networkResponse || caches.match('/offline.html');
     } catch (err) {
-        console.error(`Error fetching from network,`, request, err);
-        // return new Response(`Error fetching from network, `, err);
-        const cache = await caches.open(staticCacheName)
-        return cache.match('/offline.html')
+        // console.error(`Error fetching from network,`, request, err);
+        return new Response(`Error fetching from network, `, err);
+        // return caches.match('/offline.html')
     }
 }
