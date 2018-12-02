d7528614ea2750dc87a7482aa9e7d3e25c0860af
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-12-02 07:37:01 -0800

    Adjust image sizes since max is width is smaller than 800 in app

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,19 +10,19 @@ export function jpgImages() {
         .pipe(responsive({
             // Resize all jpg images to three different sizes: 300, 600 and 800
             '**/*.jpg': [{
-                width: 800,
+                width: 600,
                 quality: 70,
                 rename: {
                     suffix: '-large'
                 }
             }, {
-                width: 600,
+                width: 400,
                 quality: 50,
                 rename: {
                     suffix: '-medium'
                 }
             }, {
-                width: 300,
+                width: 200,
                 quality: 40,
                 rename: {
                     suffix: '-small'
