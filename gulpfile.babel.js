883a483acee8e4b70b0a8b32762615f752d686b6
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2018-12-02 05:52:43 -0800

    Add responsive images

---
+++ gulpfile.babel.js
@@ -0,0 +1,58 @@
+'use strict';
+
+import gulp, { series, parallel } from 'gulp';
+import responsive from 'gulp-responsive';
+import del from 'del';
+
+// Create responsive images for jpg files
+export function jpgImages() {
+    return gulp.src('src/images/**/*.jpg')
+        .pipe(responsive({
+            // Resize all jpg images to three different sizes: 300, 600 and 800
+            '**/*.jpg': [{
+                width: 800,
+                quality: 70,
+                rename: {
+                    suffix: '-large'
+                }
+            }, {
+                width: 600,
+                quality: 50,
+                rename: {
+                    suffix: '-medium'
+                }
+            }, {
+                width: 300,
+                quality: 40,
+                rename: {
+                    suffix: '-small'
+                }
+            }]
+        },))
+        .pipe(gulp.dest('src/img/'));
+}
+;
+
+// Just copy any other images to img folder
+export function otherImages() {
+    return gulp.src(['!images/**/*.jpg', 'images/**/*.*'])
+        .pipe(gulp.dest('img/'));
+}
+;
+
+// clean img folder
+export function clean() {
+    return del(['img/']);
+}
+;
+
+// // Run this task for your images.
+// gulp.task("images", function(done) {
+//     series(
+//         'clean',
+//         ['jpg-images', 'other-images'],
+//         done
+//     );
+// });
+
+export default series(clean, parallel(jpgImages, otherImages))
